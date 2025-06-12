7e291fb8470501ade808e8e92131c18b65c8ebe0
Anthony Nnanna <anthonynnanna004@gmail.com>
2025-06-12 20:21:04 +0100

    why us

--- resources/views/homepages/why-us.blade.php
+++ resources/views/homepages/why-us.blade.php
@@ -1,3 +1,116 @@
 <x-home-layout>
-    <div></div>
+    {{-- first div --}}
+    <div style="background-image: url('{{ asset('img/nightlife.png') }}')" class="marijuana-first-div-con">
+        <div class="marijuana-first-div-text">
+            THE ADVANTAGE
+        </div>
+    </div>
+
+    {{-- second div --}}
+    <div class="general-div-container bg-white">
+        <div class=" text-center text-4xl font-bold text-black py-4">
+            NO COMMISSIONS. NO FEES. <br> NO HASSLE. NO BRAINER
+        </div>
+        <div class="why-us-second-div-text">
+            We know that hidden fees, surprise costs and unexpected charges can take a good chunk out of your investment.
+        </div>
+        <div class="why-us-second-div-text">
+            With our VIP Black account you can trade with zero commissions and razor-thin spreads on all markets, resulting in tremendous savings.
+        </div>
+        <div class="why-us-second-div-btn button">
+            Register
+        </div>
+    </div>
+
+    {{-- third div --}}
+    <div class="general-div-container">
+        <div class="md:flex justify-between items-start gap-7">
+            <div class="basis-1/2">
+                <div class="why-us-third-div-header">EXPERIENCE</div>
+                <div class="why-us-third-div-text">
+                    A team with 40+ years experience in providing the most advanced trading solutions for FX and CFDs
+                </div>
+                <div class="why-us-third-div-header">INNOVATIVE</div>
+                <div class="why-us-third-div-text">
+                    Power up your trading with innovative products like the Fundflextradess, which lets you undo losing trades if the marketsove against you.
+                </div>
+            </div>
+            <div class="basis-1/2">
+                <img src="{{asset('img/why-us-img1.jpg')}}" alt="">
+            </div>
+        </div>
+        <div class="md:flex justify-between items-start gap-7 pt-48 pb-20">
+            <div class="basis-1/2">
+                <img src="{{asset('img/why-us-img2.jpg')}}" alt="">
+            </div>
+            <div class="basis-1/2">
+                <div class="why-us-third-div-header">PREDICTABLE PRICING</div>
+                <div class="why-us-third-div-text">
+                    One low, all-in fee. Know what you’re paying at all times with commissions as low as 0 and no fees for transactions or hidden charges.
+                </div>
+                <div class="why-us-third-div-header">ROBUST FOUNDATION</div>
+                <div class="why-us-third-div-text">
+                    All of our products and services are offered on top of a no-compromise approach to a secure, fast and dependable trading environment.
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- fourth div --}}
+    <div class="general-div-container bg-white">
+        <div class="text-center text-3xl md:text-4xl font-bold py-5">
+            AN EXCELLENT TRADING ENVIRONMENT <br> FOR FX & CFDS
+        </div>
+        <div class="font-nunito text-center text-lg md:text-xl text-gray-500">
+            Focused on security, market access, and unrivaled client support.
+        </div>
+        <div class="md:flex justify-between items-start gap-10 py-20">
+            <div class="why-us-fourth-div-card">
+                <div class="why-us-fourth-div-card-icon-con">
+                    <span class="icon-[streamline-freehand--office-stamp-document]"></span>
+                </div>
+                <div class="platform-fourth-div-card-header">LICENSED & REGULATED</div>
+                <div class="why-us-fourth-div-card-text">
+                    We also offer negative balance protection, meaning your balance never goes below zero and ensuring a high-standard of consumer protection.
+                </div>
+            </div>
+            <div class="why-us-fourth-div-card">
+                <div class="why-us-fourth-div-card-icon-con">
+                    <span class="icon-[streamline-freehand--crypto-currency-bitcoin-network-globe]"></span>
+                </div>
+                <div class="platform-fourth-div-card-header">CRYPTO DEPOSITS</div>
+                <div class="why-us-fourth-div-card-text">
+                    We accept deposits and withdrawals in Bitcoin, Ethereum and other coins.
+                </div>
+            </div>
+            <div class="why-us-fourth-div-card">
+                <div class="why-us-fourth-div-card-icon-con">
+                    <span class="icon-[streamline-freehand--messages-bubble-square-settings]"></span>
+                </div>
+                <div class="platform-fourth-div-card-header">RESPONSIVE SUPPORT</div>
+                <div class="why-us-fourth-div-card-text">
+                    Our dedicated support is quick to help you with any and all questions during trading hours.
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- fifth div --}}
+    <div class="general-div-container text-white bg-black font-bold">
+        <div class="text-2xl py-4">I'LL BE SAVING THOUSANDS PER MONTH</div>
+        <div class="text-gray-800 py-5 text-7xl"><span class="icon-[fa-solid--quote-left]"></span></div>
+        <div class="font-nunito text-lg max-w-4xl">
+            For someone who trades large volumes, the money I’ll save with 0 commissions on VIP Black will make a world of difference to my balance. I’ll be saving thousands per month on commissions, hundreds more on tight spreads, all for a simple low fee every month.
+        </div>
+        <div class="text-3xl py-5">Pawel Kaczmarczyk</div>
+    </div>
+
+    {{-- sixth div --}}
+    <div>
+        <div>FREEDOM TO TRADE THEGLOBAL MARKETS</div>
+        <div>Every tick of markets around the world at your fingertips.</div>
+        <div>
+            <div></div>
+        </div>
+    </div>
 </x-home-layout>
