1466edbc9c71fd0d15b56be4ad1f89fc95f50dc8
Anthony Nnanna <anthonynnanna004@gmail.com>
2025-06-19 04:13:40 +0100

    registration frontend

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,79 +1,81 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
-</head>
-<body>
-    <div>
-        <form action="" method="POST">
-            @csrf
+<x-auth-layout>
+    <form action="" method="POST">
+        @csrf
 
-            <div>Create an Account</div>
-            <div>Sign up with your email and get started</div>
+        <div class="auth-header">Create an Account</div>
+        <div>Sign up with your email and get started.</div>
 
-            <div>Registration</div>
+        <div class="auth-sub-head-con">
+            <span class="auth-sub-head-border">_____________ </span>
+            <span class="auth-sub-head-text"> Registration </span>
+            <span class="auth-sub-head-border"> _____________</span>
+        </div>
 
-            <div>
-                <div>
-                    <input type="text" name="firstname" value="{{ old('name') }}" required placeholder="First Name">
-                </div>
-                <div>
-                    <input type="text" name="lastname" value="{{ old('name') }}" required placeholder="Last Name">
-                </div>
+        <div>
+            <div class="auth-input-div">
+                <input type="text" name="firstname" value="{{ old('name') }}" required placeholder="First Name" class="auth-input">
             </div>
-            
-            <div>
-                <input type="email" name="email" value="{{ old('email') }}" required placeholder="Email address">
+            <div class="auth-input-div">
+                <input type="text" name="lastname" value="{{ old('name') }}" required placeholder="Last Name" class="auth-input">
             </div>
+        </div>
+        
+        <div class="auth-input-div">
+            <input type="email" name="email" value="{{ old('email') }}" required placeholder="Email address" class="auth-input">
+        </div>
 
-            <div>
-                <div>
-                    <select id="countrySelect" name="country" required>
-                        <option value="" disabled selected>Select your country</option>
-                    </select>
-                </div>
-                <div>
-                    <input type="tel" name="phone" id="phone-number" placeholder="phone number">
-                </div>
+        <div>
+            <div class="auth-input-div">
+                <select id="countrySelect" name="country" required class="auth-input">
+                    <option value="" disabled selected>Select your country</option>
+                </select>
             </div>
-
-            <div>
-                <div>
-                    <select name="gender" id="gender" required>
-                        <option value="" disabled selected>Gender</option>
-                        <option value="Male">Male</option>
-                        <option value="Female">Female</option>
-                        <option value="others">Others</option>
-                    </select>
-                </div>
-                <div>
-                    <input type="text" name="address" placeholder="address">
-                </div>
+            <div class="auth-input-div">
+                <input type="tel" name="phone" id="phone-number" placeholder="phone number" class="auth-input">
             </div>
+        </div>
 
-            <div>
-                <div>
-                    <input type="password" name="password" required placeholder="Password">
-                </div>
-                <div>
-                    <input type="password" name="password_confirmation" required placeholder="Confirm Password">
-                </div>
+        <div>
+            <div class="auth-input-div">
+                <select name="gender" id="gender" required class="auth-input">
+                    <option value="" disabled selected>Gender</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                    <option value="others">Others</option>
+                </select>
             </div>
-            
-            <button type="submit" class="btn mt-4">Register</button>
+            <div class="auth-input-div">
+                <input type="text" name="address" placeholder="address" class="auth-input">
+            </div>
+        </div>
 
-            <!-- validate errors (Laravel automatically returns this if there were errors in the controller validation) -->
-            @if($errors->any())
-                <ul class="px-4 py-2 bg-red-100">
-                    @foreach($errors->all() as $error)
-                        <li class="my-2 text-red-500">{{ $error }}</li>
-                    @endforeach
-                </ul>
-            @endif
-        </form>
-    </div>
-</body>
-</html>
+        <div>
+            <div class="auth-input-div">
+                <input type="password" name="password" required placeholder="Password" class="auth-input">
+            </div>
+            <div class="auth-input-div">
+                <input type="password" name="password_confirmation" required placeholder="Confirm Password" class="auth-input">
+            </div>
+        </div>
+        
+        <div class="auth-input-div">
+            <button type="submit" class="auth-form-btn button">Sign up</button>
+        </div>
+
+        <div class="auth-input-div">
+            <span>Already have an Account? </span>
+            <a href="{{ route('show.login') }}" class="text-blue-600 font-bold">
+                <span class="">Sign in</span>
+            </a>
+        </div>
+
+        <!-- validate errors (Laravel automatically returns this if there were errors in the controller validation) -->
+        @if($errors->any())
+            <ul class="px-4 py-2 bg-red-100">
+                @foreach($errors->all() as $error)
+                    <li class="my-2 text-red-500">{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+    </form>
+</x-auth-layout>
